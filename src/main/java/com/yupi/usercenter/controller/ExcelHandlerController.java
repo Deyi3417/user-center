f9fe6e5ff5ef4c6e9c9c9cd3cbfbcf1db019919d
Deyi3417 <18811553417@163.com>
2022-11-04 10:16:52 +0800

    component组件

---
+++ src/main/java/com/yupi/usercenter/controller/ExcelHandlerController.java
@@ -0,0 +1,18 @@
+package com.yupi.usercenter.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : HP
+ * @date : 2022/10/27
+ */
+@Slf4j
+@RestController
+@Api(tags = "ExcelHandlerController")
+@RequestMapping("/learn")
+public class ExcelHandlerController {
+
+}
